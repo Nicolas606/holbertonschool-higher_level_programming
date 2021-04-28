7826aefce964ac4684664e1179839fcf3a6e4a1c
Nicolas <castillonicolas606@gmail.com>
2021-04-28 18:07:59 +0000

    Write a program that imports functions from the file calculator_1.py

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import calculator_1 as c
+
+
+def star():
+	a = 10
+	b = 5
+	print("{} + {} = {}".format(a, b, c.add(a, b)))
+	print("{} - {} = {}".format(a, b, c.sub(a, b)))
+	print("{} * {} = {}".format(a, b, c.mul(a, b)))
+	print("{} / {} = {}".format(a, b, c.div(a, b)))
+
+if __name__ == "__main__":
+    star()
