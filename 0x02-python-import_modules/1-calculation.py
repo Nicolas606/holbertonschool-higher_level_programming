72b1be799fa7de67cd1f5bab2c564519bc1f505d
Nicolas <castillonicolas606@gmail.com>
2021-04-28 18:41:57 +0000

    fix the import

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
-import calculator_1 as c
-
-
-def star():
-	a = 10
-	b = 5
-	print("{} + {} = {}".format(a, b, c.add(a, b)))
-	print("{} - {} = {}".format(a, b, c.sub(a, b)))
-	print("{} * {} = {}".format(a, b, c.mul(a, b)))
-	print("{} / {} = {}".format(a, b, c.div(a, b)))
+from calculator_1 import add, sub, mul, div
 
 if __name__ == "__main__":
-    star()
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
