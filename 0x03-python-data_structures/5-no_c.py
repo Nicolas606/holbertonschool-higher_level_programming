01a8ce8a4b68787e3b441ec5e07c7f67a2dc1dd4
Nicolas <castillonicolas606@gmail.com>
2021-05-06 03:58:16 +0000

    Fix the new string

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    delete = {67 : 26, 99 : 26}
-    return my_string.translate(delete)
+    delete = {67: 26, 99: 26}
+    new_string = my_string.translate(delete)
+    return new_string
