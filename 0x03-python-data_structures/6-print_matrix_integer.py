4294533a20b24d0b6d52dc7700093c1449e56878
Nicolas <castillonicolas606@gmail.com>
2021-05-06 03:03:07 +0000

    Fix the {:d} in the pront

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-
     for first_matrix in matrix:
         for second_matrix in first_matrix:
             if second_matrix != first_matrix[-1]:
-                print("{} ".format(second_matrix), end='')
+                print("{:d} ".format(second_matrix), end='')
             else:
-                print("{}".format(second_matrix), end='')
+                print("{:d}".format(second_matrix), end='')
         print('')
