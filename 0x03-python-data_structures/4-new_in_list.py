a0e320db751860d5a68ab895fe49c1754de91fea
Nicolas <castillonicolas606@gmail.com>
2021-05-06 02:47:10 +0000

    Fix the length - 1

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,10 +2,7 @@
 def new_in_list(my_list, idx, element):
     copy = []
     length = len(my_list)
-
-    for i in range(length):
+    for i in range(length - 1):
         copy.append(my_list[i])
-
     copy[idx] = element
-
-    return copy
+    return (copy)
