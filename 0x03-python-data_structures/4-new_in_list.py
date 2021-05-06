b0215aa14e2054cd32236abfa31ca3efaefd52c7
Nicolas <castillonicolas606@gmail.com>
2021-05-06 02:48:59 +0000

    Fix range

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,7 +2,7 @@
 def new_in_list(my_list, idx, element):
     copy = []
     length = len(my_list)
-    for i in range(length - 1):
+    for i in range(length):
         copy.append(my_list[i])
     copy[idx] = element
     return (copy)
