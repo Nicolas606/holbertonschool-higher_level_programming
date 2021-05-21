7e93cdef2d329f5a34819cf16cdd1596a37b3ee5
Nicolas <castillonicolas606@gmail.com>
2021-05-21 15:27:36 +0000

    Fix

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,8 +6,8 @@ class Rectangle:
     """ Class: For defining a Rectangule object. """
     def __init__(self, width=0, height=0):
         """ Method that initializes the class """
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -10,8 +10,8 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """ Method that initializes the class """
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
         Rectangle.number_of_instances += 1
 
     @property
