afc516617aa07344b2dbf64cfedd696551aa69e5
Nicolas <castillonicolas606@gmail.com>
2021-06-01 02:24:51 +0000

    Fis the read un the print

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,4 +6,4 @@ def read_file(filename=""):
     """ function that reads a text file (UTF8) and prints it to stdout """
 
     with open(filename, 'r', encoding='utf-8') as new_file:
-        print(new_file, end='')
+        print(new_file.read(), end='')
