d617809976ce0262efb9f794e9f44136d26acbc5
Nicolas <castillonicolas606@gmail.com>
2021-06-01 02:34:34 +0000

    Fix pep8

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -5,7 +5,7 @@ import json
 
 
 def save_to_json_file(my_obj, filename):
-    """  function that writes an Object to a text file, using a JSON 
+    """  function that writes an Object to a text file, using a JSON
 representation """
     with open(filename, 'w') as new_file:
         json.dump(my_obj, new_file)
