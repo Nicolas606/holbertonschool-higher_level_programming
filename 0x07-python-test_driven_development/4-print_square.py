06711530e24a51b0c126bfd12a024270a33bb0b6
Nicolas <castillonicolas606@gmail.com>
2021-06-02 02:15:33 +0000

    Fix pep8

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -6,7 +6,7 @@ def print_square(size):
     """ function that prints a square with the character # """
     if type(size) != int or (type(size) == float and size < 0):
         raise TypeError('size must be an integer')
-    
+
     if size < 0:
         raise ValueError('size must be >= 0')
 
