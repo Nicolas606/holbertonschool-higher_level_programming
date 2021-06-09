bbd40cb24eee508c2cb19f5ab4fb7c6232a13785
Nicolas <castillonicolas606@gmail.com>
2021-06-09 02:53:36 +0000

    Fix pep8

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -5,6 +5,7 @@
 class Base:
     """ This class will be the “base” of all other classes in this project."""
     __nb_objects = 0
+
     def __init__(self, id=None):
         if id is not None:
             self.id = id
