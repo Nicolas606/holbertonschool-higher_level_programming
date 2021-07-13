229e55f757c385da867c5dae6260aca45d448acf
Nicolas606 <castillonicolas606@gmail.com>
2021-07-13 18:41:25 -0500

    script that creates the database hbtn_0d_usa and the table states

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+--  script that creates the database hbtn_0d_usa and the table states
+CREATE database IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE table IF NOT EXISTS states (
+    id INT AUTO_INCREMENT UNIQUE NOT NULL PRIMARY KEY,
+    name VARCHAR(256) NOT NULL
+)
