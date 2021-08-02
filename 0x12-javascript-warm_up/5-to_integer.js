26b056c2e8938152b91f8a9e2b792ee9e7c77e43
Nicolas606 <castillonicolas606@gmail.com>
2021-08-02 16:04:06 -0500

    Converted the argument to an integer

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const argument = process.argv;
+const num = parseInt(argument[2], 10);
+if (isNaN(num)) {
+  console.log('Not a number');
+} else {
+  console.log('My number: ' + num);
+}
