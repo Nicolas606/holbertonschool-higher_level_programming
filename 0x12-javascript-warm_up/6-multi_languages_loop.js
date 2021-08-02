0104e2cbbf0a661ee7a75df51a0cd8bcd1e36868
Nicolas606 <castillonicolas606@gmail.com>
2021-08-02 16:13:02 -0500

    Fix the typos

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const languajes = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const languajes = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (const i in languajes) {
   console.log(languajes[i]);
 }
