43d2e97bbabfd6064c3821f59f6f2c7a0dae8fdf
Nicolas606 <castillonicolas606@gmail.com>
2021-08-13 18:46:42 -0500

    Fix pep8

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -8,18 +8,15 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 if __name__ == '__main__':
 
-    mysql_uname = argv[1]
-    mysql_upass = argv[2]
-    mysql_db = argv[3]
+    uname = argv[1]
+    upass = argv[2]
+    db = argv[3]
     name = argv[4]
-    u_host = "localhost"
 
     # dialect+driver://username:password@host:port/database
-
-    engine = create_engine("mysql+mysqldb://{}:{}@{}:3306/{}".
-                           format(mysql_uname, mysql_upass, u_host, mysql_db), pull_pre_ping=True)
-
-    #link to our engine
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".
+                           format(uname, upass, db), pool_pre_ping=True)
+    """ link to our engine """
     Session = sessionmaker(bind=engine)
     session = Session()
 
