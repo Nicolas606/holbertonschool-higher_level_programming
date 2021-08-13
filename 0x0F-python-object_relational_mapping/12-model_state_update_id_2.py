0f4330674081d65d881e2fca87f8a2177a38b10b
Nicolas606 <castillonicolas606@gmail.com>
2021-08-13 18:51:10 -0500

    Fix pep8

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -7,17 +7,15 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 if __name__ == '__main__':
 
-    mysql_uname = argv[1]
-    mysql_upass = argv[2]
-    mysql_db = argv[3]
-    u_host = "localhost"
+    uname = argv[1]
+    upass = argv[2]
+    db = argv[3]
 
     # dialect+driver://username:password@host:port/database
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".
+                           format(uname, upass, db), pool_pre_ping=True)
 
-    engine = create_engine("mysql+mysqldb://{}:{}@{}:3306/{}".
-                           format(mysql_uname, mysql_upass, u_host, mysql_db), pull_pre_ping=True)
-
-    #link to our engine
+    """link to our engine """
     Session = sessionmaker(bind=engine)
     session = Session()
 
