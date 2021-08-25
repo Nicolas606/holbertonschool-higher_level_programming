34f9d748c8751472c07c352459e54a1e96f4878a
Nicolas606 <castillonicolas606@gmail.com>
2021-08-24 21:48:58 -0500

    Fix pep8

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" script that takes your GitHub credentials (username and password) and uses the GitHub API to display your id  """
+""" script that takes your GitHub credentials (username and password) and uses
+the GitHub API to display your id  """
 import requests
 from sys import argv
 
@@ -8,7 +9,7 @@ if __name__ == "__main__":
     userpass = argv[2]
     url = 'https://api.github.com/user'
 
-    req = requests.get( url, auth=(userName, userpass))
+    req = requests.get(url, auth=(userName, userpass))
     try:
         print(req.json().get('id'))
     except:
