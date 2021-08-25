91c80894c10170ff413dc6d8122067d875d325ee
Nicolas606 <castillonicolas606@gmail.com>
2021-08-24 21:21:14 -0500

    Upload task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" script that takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user with the letter as a parameter. """
+if __name__ == '__main__':
+    import requests
+    from sys import argv
+
+    if len(argv) < 2:
+        print('No result')
+    else:
+        q = argv[1]
+        data = {'q': q}
+        url = 'http://0.0.0.0:5000/search_user'
+        req = requests.post(url, data)
+        try:
+            result = req.json()
+            if result:
+                print("[{}] {}".format(result.get('id'), result.get('name')))
+            else:
+                print("No result")
+        except:
+            print("Not a valid JSON")
