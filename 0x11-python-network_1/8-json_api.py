1d5aa45e662e61d6e8f2fb4d577228face995e09
Nicolas606 <castillonicolas606@gmail.com>
2021-08-24 22:18:10 -0500

    Fix pep8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-""" script that takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user with the letter as a parameter. """
+""" script that takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter. """
+import requests
+from sys import argv
+
 if __name__ == '__main__':
-    import requests
-    from sys import argv
 
     if len(argv) < 2:
         print('No result')
