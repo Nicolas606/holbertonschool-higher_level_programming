30d2cc60dfff8ebbd2692e71a637ca234b92fd0d
Nicolas606 <castillonicolas606@gmail.com>
2021-09-15 17:25:09 -0500

    script that reads and prints the content of a file

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+/* script that reads and prints the content of a file */
+
+const fs = require ('fs')
+const archivo = process.argv[2]
+
+fs.readFile (archivo, 'utf8', (err, jsonString) => {
+    if (err) {
+        console.log (err)
+        return
+    }
+    console.log (jsonString)
+})
