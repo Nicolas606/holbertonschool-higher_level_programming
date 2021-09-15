9c118cdd9452cca4bb5a021831a5837f4d65343a
Nicolas606 <castillonicolas606@gmail.com>
2021-09-15 17:38:17 -0500

    script that writes a string to a file.

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+/* script that writes a string to a file. */
+
+const fs = require('fs');
+const archivo = process.argv[2];
+const data = process.argv[3];
+
+fs.writefile(archivo,data, 'utf8', (err) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+});
