52348ed42593432dc92ab6ca84b9f4d24f1557aa
Nicolas606 <castillonicolas606@gmail.com>
2021-09-15 23:05:32 -0500

    script that prints all characters of a Star Wars movie:

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+/*  script that prints all characters of a Star Wars movie */
+
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+
+const request = require('request');
+request(url, function (error, response) {
+  if (error) {
+    console.log(error);
+  }
+  const characters = (JSON.parse(response.body).characters)
+  characters.forEach(element => {
+    const people = element;
+    request(people, function (error, response) {
+      if (error) {
+        console.log(error);
+      }
+      const result = (JSON.parse(response.body).name)
+      console.log(result);
+    });
+  })
+});
