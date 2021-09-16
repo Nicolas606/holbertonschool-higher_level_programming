d0efdb9ab1760f6cb35b48b0e26923d9609be8ed
Nicolas606 <castillonicolas606@gmail.com>
2021-09-15 22:26:59 -0500

    script that computes the number of tasks completed by user id.

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+/* script that computes the number of tasks completed by user id. */
+
+const request = require('request');
+const url = process.argv[2];
+const list = {};
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  }
+  const data = JSON.parse(response.body);
+  data.forEach(element => {
+    if (element.completed === true) {
+      list[element.userId] += 1;
+      if (isNaN(list[element.userId])){
+        list[element.userId] = 1;
+      }
+    }
+  });
+  console.log(list);
+});
